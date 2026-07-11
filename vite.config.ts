2a220a931e1136d89f6dcf5399d794df97c3b632
Aadit Bhatia <bhatia.aadit@gmail.com>
2026-07-10 23:49:15 -0400

    Fix to prod functionality

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: 'Personal-Portfolio-React-',
   plugins: [react()], // or vue(), svelte(), etc.
-})   
+})
+
